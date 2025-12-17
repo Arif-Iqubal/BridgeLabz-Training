c7ca456f3061836bd34fb8aa357b2a945fbf3b54
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-17 17:08:17 +0530

    [Arif Iqubal] : added kilometer to miles conversion program

---
+++ gcr-codebase/programming-elements/level-1/KilometersToMiles.java
@@ -0,0 +1,24 @@
+//Creating a class name KilometersToMiles to convert distance from kilometer to miles
+
+public class KilometersToMiles{
+
+	public static void main(String[] args){
+	
+	// Creating a variable double and storing the distance in kilometers
+	double distance = 10.8;
+	
+	
+	
+	// Converting kilometer to Miles
+	double miles = distance * 1.6 ;
+	
+	// Display the distance in miles
+	System.out.println("The distance " + distance + " km in miles is " + miles);
+	
+	
+	}
+
+
+
+
+} 
