ff4a2308b5e58af90149c26531dac18d1e638166
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-18 22:22:55 +0530

    [Arif Iqubal] : added a program to check leap year

---
+++ gcr-codebase/control-flow/level-3/IsLeapYear.java
@@ -0,0 +1,55 @@
+//importing Scanner class from java.util package
+import java.util.Scanner;
+
+//Creating a class name IsLeapYear and calculate and display whether the year is leap or not
+public class IsLeapYear{
+
+	public static void main(String[] args){
+	
+		
+	// Creating object of class Scanner
+	Scanner input = new Scanner( System.in);
+	
+	// Taking user input for year
+	System.out.println("Enter a year : ");
+	int year = input.nextInt();
+	
+	//checking the year is greater than 1581 or not
+	if(year >= 1582){
+
+		// checking the year is a leap year or not
+		if( year % 4 == 0){
+			if( year % 100 == 0){
+				if( year % 400 == 0){
+					System.out.println("The year " + year + " is a leap year");
+				}
+				else{
+					System.out.println("The year " + year + " is not a leap year");
+				}
+			}
+			else{
+				System.out.println("The year " + year + " is a leap year");
+			}
+		
+		}
+		else{
+			System.out.println("The year " + year + " is not a leap year");
+		}
+			
+	}
+	else{
+		System.out.println("The year " + year + " is lower than 1582 ");
+		
+	}
+
+	//close the Scanner object
+	input.close();
+	
+	
+	
+	}
+
+
+
+
+} 
