8c20f2f2ea6f66ca26556e1ea4704b9d4a31da53
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-18 17:00:49 +0530

    [Arif Iqubal] : added a program to display all multiple of a number using for loop

---
+++ gcr-codebase/control-flow/level-2/AllMultiplesForLoop.java
@@ -0,0 +1,45 @@
+//importing Scanner class from java.util package
+import java.util.Scanner;
+
+//Creating a class name AllMultiplesForLoop and displaying all the multiples of the given number
+public class AllMultiplesForLoop{
+
+	public static void main(String[] args){
+	
+		
+	// Creating object of class Scanner
+	Scanner input = new Scanner( System.in);
+	
+	// Taking user input for a number
+	System.out.println("Enter a number : ");
+	int number = input.nextInt();
+
+	//checking the number is positive and smaller than 100
+	if((number >= 0) && (number < 100)){
+
+		//Displaying the multiple of number from 1 to given number
+		for(int i = number  ; i > 0 ; i--){
+			
+			if(number % i == 0){
+				System.out.println("The number " + i + " is the multiple of " + number);
+			}
+			
+		}
+		
+	}
+	else{
+		System.out.println("The number " + number + " is not positive or, greater or equal to 100");
+		
+	}
+
+	//close the Scanner object
+	input.close();
+	
+	
+	
+	}
+
+
+
+
+} 
