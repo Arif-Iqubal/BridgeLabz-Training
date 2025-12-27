e5c9c5cd2a498cca03da3f33341fdb8ab3eddd9c
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-27 16:18:08 +0530

    [Arif Iqubal] : added a program to access public and protected instance of Employee class

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructorandinstance/level1/Manager.java
@@ -0,0 +1,22 @@
+package com.constructorandinstance.level1;
+
+public class Manager extends Employee {
+
+	Manager(String employeeID, String department, int salary) {
+		super(employeeID, department, salary);
+		
+	}
+
+	public static void main(String[] args) {
+		// Creating object of manager Class
+		Manager manager1 = new Manager("237A","Front-End",9000);
+		
+		//Accessing public instance of parent class
+		System.out.println("Assess public instance of parent class \n Employee ID => " + manager1.employeeID);
+
+		//Accessing protected instance of parent class
+		System.out.println("Assess protected instance of parent class \n Department => " + manager1.department);
+
+	}
+
+}
