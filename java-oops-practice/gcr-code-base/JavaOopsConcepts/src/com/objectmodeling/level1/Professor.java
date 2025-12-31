c62b4c68d7e56350a457956244dddb8f3756d046
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 15:19:11 +0530

    [Arif Iqubal] : added a program that creates a Professor class

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Professor.java
@@ -0,0 +1,13 @@
+package com.objectmodeling.level1;
+
+//Created a Professor class
+public class Professor {
+	//private instance of class
+    private String name;
+
+    //Parameterized constructor
+    public Professor(String name) {
+        this.name = name;
+    }
+}
+
