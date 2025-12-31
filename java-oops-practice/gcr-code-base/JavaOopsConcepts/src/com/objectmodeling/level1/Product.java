c5d8eeec641af994fc9007b4a69b681721eae6c6
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 15:08:59 +0530

    [Arif Iqubal] : added a program that creates a product class that add items

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Product.java
@@ -0,0 +1,11 @@
+package com.objectmodeling.level1;
+//Created a product class
+public class Product {
+	//Private instance of class
+    private String productName;
+
+    //Parameterized constructor
+    public Product(String productName) {
+        this.productName = productName;
+    }
+}
