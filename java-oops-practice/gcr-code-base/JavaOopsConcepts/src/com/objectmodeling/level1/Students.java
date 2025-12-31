f75d3e4d2c7107022fc236c64488cd2c269c558b
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 15:19:31 +0530

    [Arif Iqubal] : added a program that creates a Student class

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Students.java
@@ -0,0 +1,9 @@
+package com.objectmodeling.level1;
+
+//Created a Students class
+public class Students {
+	//public method to enroll student in courses
+    public void enrollCourse(Courses course) {
+        System.out.println("Student enrolled in " + course.getName());
+    }
+}
