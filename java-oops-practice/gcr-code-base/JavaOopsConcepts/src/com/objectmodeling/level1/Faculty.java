299cbc78919d4b8e175e3bcf8b729aea014335d2
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 14:44:21 +0530

    [Arif Iqubal] : added a program that creates a Faculty class

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Faculty.java
@@ -0,0 +1,13 @@
+package com.objectmodeling.level1;
+
+//Create a class Faculty
+public class Faculty {
+	//Private instance of class
+    private String name;
+    
+    //Parameterized Constructor
+    public Faculty(String name) {
+        this.name = name;
+    }
+}
+
