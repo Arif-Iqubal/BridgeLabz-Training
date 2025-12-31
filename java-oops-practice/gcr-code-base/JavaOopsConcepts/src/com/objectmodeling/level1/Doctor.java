8ea24721509d57d06548f2d8c68df1ca3e10109e
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 14:59:40 +0530

    [Arif Iqubal] : added a program that creates a Doctor class

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Doctor.java
@@ -0,0 +1,17 @@
+package com.objectmodeling.level1;
+
+//Created a doctor class
+public class Doctor {
+	//private instance of class
+    private String name;
+    
+    //Parameterized constructor
+    public Doctor(String name) {
+        this.name = name;
+    }
+
+    //public method to consult patient
+    public void consult(Patient patient) {
+        System.out.println("Dr. " + name + " consulting patient " + patient.getName());
+    }
+}
