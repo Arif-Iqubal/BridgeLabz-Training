ea390cca717e0b9253464b7016bc0b17c7bbf626
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 15:07:48 +0530

    [Arif Iqubal] : added a program that creates a Customers class that place products

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Customers.java
@@ -0,0 +1,9 @@
+package com.objectmodeling.level1;
+//Create a class customer
+public class Customers {
+	//Public method that display order placed
+    public void placeOrder(Order order) {
+        System.out.println("Order placed successfully.");
+    }
+}
+
