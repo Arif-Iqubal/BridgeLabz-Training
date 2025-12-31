168a7e29c3720ce73fa33fee4332f00d0e8cd9b6
Arif-Iquba'l <arif04102001@gmail.com>
2025-12-31 14:13:51 +0530

    [Arif Iqubal] : added a program which create a employee class

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/level1/Employee.java
@@ -0,0 +1,13 @@
+package com.objectmodeling.level1;
+
+public class Employee {
+    private String employeeName;
+
+    public Employee(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+}
