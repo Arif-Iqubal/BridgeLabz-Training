795bee42f30b7632d3a52b8fa5886a8bcca43a19
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-01 22:30:36 +0530

    [Arif Iqubal] : added a program of School management system

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/hierarchical/schoolsystem/Person.java
@@ -0,0 +1,14 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+//Super class
+public class Person {
+    protected String name;
+    protected int age;
+    
+    //Parameterized constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
