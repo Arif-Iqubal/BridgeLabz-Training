8ce6d044fcce784494fa5a49096559f86523a922
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-01 22:28:36 +0530

    [Arif Iqubal] : added a program that display details of online retails shop

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/multilevel/onlineretail/Order.java
@@ -0,0 +1,17 @@
+package com.inheritance.multilevel.onlineretail;
+//Super class
+public class Order {
+	protected String orderId;
+	protected String orderDate;
+	
+	//Parameterized constructor
+	public Order(String orderId, String orderDate) {
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+	}
+	
+	public void getOrderStatus() {
+		System.out.println("Showing Order Status");
+	}
+	
+}
