73fe7cd356022cf7ec639d8aa6cf42dfd36e57bf
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-04 20:20:27 +0530

    [Arif Iqubal] : added a program of inventory management system

---
+++ dsa-practice/gcr-code-base/JavaDsaConcepts/src/com/linkedlist/singlelinkedlist/inventorymanagement/ItemNode.java
@@ -0,0 +1,21 @@
+package com.linkedlist.singlelinkedlist.inventorymanagement;
+
+//Represents a single item in the inventory (Node of Singly Linked List)
+public class ItemNode {
+
+ int itemId;
+ String itemName;
+ int quantity;
+ double price;
+ ItemNode next;
+
+ // Constructor
+ public ItemNode(int itemId, String itemName, int quantity, double price) {
+     this.itemId = itemId;
+     this.itemName = itemName;
+     this.quantity = quantity;
+     this.price = price;
+     this.next = null;
+ }
+}
+
