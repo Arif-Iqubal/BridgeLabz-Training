b24b29a958e040f96300b21086ec6c31d1eae9e6
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-07 22:37:29 +0530

    [Arif Iqubal] : solved a scenario based question of book bazaar

---
+++ java-oops-practice/scenario-based/bookbazaar/BookBazaarApp.java
@@ -0,0 +1,38 @@
+package bookbazaar;
+
+//Main class to test BookBazaar system
+public class BookBazaarApp {
+
+ public static void main(String[] args) {
+
+     // Create books
+     Book ebook = new EBook(
+             "Clean Code",
+             "Robert C. Martin",
+             500,
+             50
+     );
+
+     Book printedBook = new PrintedBook(
+             "Java: The Complete Reference",
+             "Herbert Schildt",
+             800,
+             30
+     );
+
+     // Create order
+     Order order = new Order("Arif Iqubal");
+
+     // Add books to order
+     order.addBook(ebook, 2);        // price × quantity − discount
+     order.addBook(printedBook, 1);
+
+     // Confirm order
+     order.confirmOrder();
+
+     System.out.println();
+
+     // Display order summary
+     order.showOrderSummary();
+ }
+}
