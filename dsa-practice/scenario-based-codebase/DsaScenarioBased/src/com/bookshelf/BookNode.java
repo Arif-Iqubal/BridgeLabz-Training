87eca61e0e9a41d608b1c7719a57eb83ad968f32
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-10 22:01:35 +0530

    [Arif Iqubal] : solved dsa scenario based question of book self in library

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/bookshelf/BookNode.java
@@ -0,0 +1,21 @@
+package com.bookshelf;
+//Book Node to store books details
+public class BookNode {
+	//BookNode instance
+	String title;
+	String author;
+	String genre;
+	
+	//Parameterized constructor
+	public BookNode(String title, String author, String genre) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+	}
+	
+	
+	
+	
+	
+}
