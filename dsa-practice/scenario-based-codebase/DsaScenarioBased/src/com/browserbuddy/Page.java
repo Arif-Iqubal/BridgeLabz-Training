b21f9c11d9788f4d6b23eb1d51b1a885a1c36f43
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-10 22:03:24 +0530

    [Arif Iqubal] : solved dsa scenario based question of browser page forward and backward

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/browserbuddy/Page.java
@@ -0,0 +1,14 @@
+package com.browserbuddy;
+
+public class Page {
+	String currentPage;
+	Page next;
+	Page prev;
+	public Page(String currentPage) {
+		super();
+		this.currentPage = currentPage;
+		this.next = null;
+		this.prev = null;
+	}
+	
+}
