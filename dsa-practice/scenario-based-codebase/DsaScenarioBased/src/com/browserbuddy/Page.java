276cc39a8ac0480141695a55c21fae116a963c6c
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-12 21:12:37 +0530

    [Arif Iqubal] : updated browser buddy page node

--- dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/browserbuddy/Page.java
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/browserbuddy/Page.java
@@ -1,9 +1,12 @@
 package com.browserbuddy;
 
+//Page Node
 public class Page {
 	String currentPage;
 	Page next;
 	Page prev;
+	
+	//Constructor
 	public Page(String currentPage) {
 		super();
 		this.currentPage = currentPage;
