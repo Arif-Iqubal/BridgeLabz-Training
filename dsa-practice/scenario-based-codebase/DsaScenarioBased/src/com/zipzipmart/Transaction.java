7c4f5b06c49eea71dd036e0db0a5b279ba117c87
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-15 22:22:53 +0530

    [Arif Iqubal] : solved dsa scenario based question of sorting transaction in a mart

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/zipzipmart/Transaction.java
@@ -0,0 +1,17 @@
+package com.zipzipmart;
+
+import java.time.LocalDate;
+
+//Class Transaction to store details of transaction
+public class Transaction {
+	LocalDate date;
+	double amount;
+
+	// Parameterized constructor
+	public Transaction(LocalDate date, double amount) {
+
+		this.date = date;
+		this.amount = amount;
+	}
+
+}
