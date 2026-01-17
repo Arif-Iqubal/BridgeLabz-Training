10a6e42b0647395471a8dc0a08b3f688a4f42599
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-17 16:21:32 +0530

    [Arif Iqubal] : solved dsa scenario based question of fleet manager

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/fleetmanager/Vehicle.java
@@ -0,0 +1,19 @@
+package com.fleetmanager;
+
+//Vehicle class to store vehicle details
+public class Vehicle {
+	// Instance of class
+	String brand;
+	String model;
+	int mileage;
+	String depot;
+
+	// Parameterized constructor
+	public Vehicle(String brand, String model, int mileage, String depot) {
+		this.brand = brand;
+		this.model = model;
+		this.mileage = mileage;
+		this.depot = depot;
+	}
+
+}
