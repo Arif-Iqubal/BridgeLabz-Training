34d2bbac2c8cafd9d63561416f91b0d2e8452e82
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-19 16:50:06 +0530

    [Arif Iqubal] : solved dsa scenario based question of flash deal using quick sort

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/flashdealz/Product.java
@@ -0,0 +1,19 @@
+package com.flashdealz;
+
+//Product class to store details of products
+public class Product {
+
+	// Instance of class
+	String product;
+	int price;
+	double discount;
+
+	// Parameterized Constructor
+	public Product(String product, int price, double discount) {
+
+		this.product = product;
+		this.price = price;
+		this.discount = discount;
+	}
+
+}
