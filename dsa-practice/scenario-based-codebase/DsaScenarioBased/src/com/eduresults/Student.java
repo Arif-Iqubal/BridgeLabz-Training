19d0239d7be4036d1dedd62de16746be458dd2bf
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-19 16:48:47 +0530

    [Arif Iqubal] : solved dsa scenario based question of edu results using mergesort

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/eduresults/Student.java
@@ -0,0 +1,17 @@
+package com.eduresults;
+
+///Class Student to store details of students
+public class Student {
+	String name;
+	int score;
+	String district;
+
+	// Parameterized Constructor
+	public Student(String name, int score, String district) {
+
+		this.name = name;
+		this.score = score;
+		this.district = district;
+	}
+
+}
