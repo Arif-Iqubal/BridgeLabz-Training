c8cb3031a1200c3b94e838352f7723a3e039cf5b
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-20 17:19:50 +0530

    [Arif Iqubal] : solved dsa scenario based question of food fest

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/foodfest/Stall.java
@@ -0,0 +1,17 @@
+package com.foodfest;
+
+//Stall class to store details of stall
+public class Stall {
+	String stallName;
+	int count;
+	String zone;
+
+	// Parameterized Constructor
+	public Stall(String stallName, int count, String zone) {
+
+		this.stallName = stallName;
+		this.count = count;
+		this.zone = zone;
+	}
+
+}
