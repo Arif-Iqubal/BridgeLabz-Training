0dcac58787628334c10e299ac05d47e1f8186c3a
Arif-Iquba'l <arif04102001@gmail.com>
2026-01-23 23:02:44 +0530

    [Arif Iqubal] : solved dsa scenario based question of hospital queue management using AVL Tree

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/hospitalqueuemangement/PatientNode.java
@@ -0,0 +1,19 @@
+package com.hospitalqueuemangement;
+
+//Node representing a patient
+public class PatientNode {
+
+	int checkInTime; // patient arrival time
+	String patientName; // patient name
+	int height; // height for AVL balancing
+
+	PatientNode left; // left child
+	PatientNode right; // right child
+
+	// Constructor
+	PatientNode(int checkInTime, String patientName) {
+		this.checkInTime = checkInTime;
+		this.patientName = patientName;
+		this.height = 1;
+	}
+}
